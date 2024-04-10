bd78c976a8ed4652ae64a16aec79debefd3ef6f5
Guilherme <guilhermersouza34@gmail.com>
2024-04-09 21:53:14 -0300

    Adicionando a imagem e atualizando o js

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var deadline = new Date('2024-06-10').getTime(); // Data do aniversário
+AOS.init();
 
-    var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = deadline - now;
+const eventDate = new Date("Mar 24, 2025 11:42:00");
+const eventTimeStamp = eventDate.getTime();
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h '
-        + minutes + 'm ' + seconds + 's ';
+const timeCounter = setInterval(function() {
+    const now = new Date();
+    const nowTimeStamp = now.getTime();
 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById('countdown').innerHTML = 'Feliz Aniversário!';
-        }
-    }, 1000);
-});
+    const eventDistance = eventTimeStamp - nowTimeStamp;
+
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const hourInMs = 1000 * 60 * 60;
+    const minInMs = 1000 * 60;
+
+    const daysToEvent = Math.floor(eventDistance / dayInMs);
+    const hoursToEvent = Math.floor(eventDistance % dayInMs / hourInMs);
+    const minutesToEvent = Math.floor(eventDistance % hourInMs / minInMs);
+    const secondsToEvent = Math.floor((eventDistance % minInMs) / 1000);
+
+    document.getElementById('counter').innerHTML = `${daysToEvent}d ${hoursToEvent}h ${minutesToEvent}m ${secondsToEvent}s`;
+
+    if (eventDistance < 0) {
+        clearInterval(timeCounter);
+        document.getElementById('counter').innerHTML = 'Too late';
+    }
+
+}, 1000)
